84ff49aad4f4c006db970cb475e7807b35d71737
Manoel Fernandes <manoelfernandes15@gmail.com>
2022-01-07 16:30:57 -0300

    first commit

---
+++ src/pages/LiveCodeScreen/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export default function LiveCodeScreen() {
+  return (
+    <Container>
+      <span>Minha tela incrível aqui</span>
+    </Container>
+  );
+}
